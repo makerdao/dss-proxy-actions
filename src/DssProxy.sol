8108b2ad54e3e4035385ac32624e6ddc53e5c30e
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2018-11-20 11:42:22 -0300

    Initial commit

--- src/DssProxy.sol
+++ src/DssProxy.sol
@@ -1,4 +1,224 @@
 pragma solidity ^0.4.24;
 
+contract GemLike {
+    function approve(address, uint) public;
+    function transferFrom(address, address, uint) public;
+}
+
+contract CdpHandlerLike {
+    function setOwner(address) public;
+    function execute(address, bytes) public returns (bytes32);
+}
+
+contract CdpRegistryLike {
+    function create() public returns (address);
+}
+
+contract PitLike {
+    function vat() public view returns (VatLike); 
+}
+
+contract VatLike {
+    function ilks(bytes32) public view returns (uint, uint);
+}
+
+contract GemJoinLike {
+    function gem() public returns(GemLike);
+}
+
+contract DaiAJoinLike {
+    function dai() public returns(GemLike);
+}
+
 contract DssProxy {
+    function open(
+        address cdpRegistry
+    ) public returns (address handler) {
+        handler = CdpRegistryLike(cdpRegistry).create();
+    }
+
+    function give(
+        address handler,
+        address guy
+    ) public {
+        CdpHandlerLike(handler).setOwner(guy);
+    }
+
+    function lockETH(
+        address handler,
+        address cdpLib,
+        address ethJoin,
+        address pit
+    ) public payable {
+        bytes memory calldata = abi.encodeWithSignature("ethJoin_join(address,bytes32)", bytes32(ethJoin), bytes32(handler));
+        assert(
+            address(handler).call.value(msg.value)(bytes4(keccak256("execute(address,bytes)")), cdpLib, uint256(0x40), calldata.length, calldata)
+        );
+
+        (uint take,) = PitLike(pit).vat().ilks("ETH");
+        calldata = abi.encodeWithSignature("frob(address,bytes32,int256,int256)", bytes32(pit), bytes32("ETH"), int(msg.value * 10 ** 27 / take), int(0));
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+    }
+
+    function lockGem(
+        address handler,
+        address cdpLib,
+        address gemJoin,
+        address pit,
+        bytes32 ilk,
+        uint wad
+    ) public {
+        GemJoinLike(gemJoin).gem().transferFrom(msg.sender, this, wad);
+        GemJoinLike(gemJoin).gem().approve(handler, wad);
+        bytes memory calldata = abi.encodeWithSignature("gemJoin_join(address,bytes32,uint256)", bytes32(gemJoin), bytes32(handler), wad);
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+
+        (uint take,) = PitLike(pit).vat().ilks(ilk);
+        calldata = abi.encodeWithSignature("frob(address,bytes32,int256,int256)", bytes32(pit), bytes32(ilk), int(wad * 10 ** 27 / take), int(0));
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+    } 
+
+    function freeETH(
+        address handler,
+        address cdpLib,
+        address ethJoin,
+        address pit,
+        uint wad
+    ) public {
+        (uint take,) = PitLike(pit).vat().ilks("ETH");
+        bytes memory calldata = abi.encodeWithSignature("frob(address,bytes32,int256,int256)", bytes32(pit), bytes32("ETH"), -int(wad * 10 ** 27 / take), 0);
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+        
+        calldata = abi.encodeWithSignature("ethJoin_exit(address,address,uint256)", bytes32(ethJoin), bytes32(msg.sender), wad);
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+    }
+
+    function freeGem(
+        address handler,
+        address cdpLib,
+        address gemJoin,
+        address pit,
+        bytes32 ilk,
+        uint wad
+    ) public {
+        (uint take,) = PitLike(pit).vat().ilks(ilk);
+        bytes memory calldata = abi.encodeWithSignature("frob(address,bytes32,int256,int256)", bytes32(pit), ilk, -int(wad * 10 ** 27 / take), 0);
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+        
+        calldata = abi.encodeWithSignature("gemJoin_exit(address,address,uint256)", bytes32(gemJoin), bytes32(msg.sender), wad);
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+    }
+
+    function draw(
+        address handler,
+        address cdpLib,
+        address daiJoin,
+        address pit,
+        bytes32 ilk,
+        uint wad
+    ) public {
+        (, uint rate) = PitLike(pit).vat().ilks(ilk);
+        bytes memory calldata = abi.encodeWithSignature("frob(address,bytes32,int256,int256)", bytes32(pit), ilk, 0, int(wad * 10 ** 27 / rate));
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+
+        calldata = abi.encodeWithSignature("daiJoin_exit(address,address,uint256)", bytes32(daiJoin), bytes32(msg.sender), wad);
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+    }
+
+    function wipe(
+        address handler,
+        address cdpLib,
+        address daiJoin,
+        address pit,
+        bytes32 ilk,
+        uint wad
+    ) public {
+        DaiAJoinLike(daiJoin).dai().transferFrom(msg.sender, this, wad);
+        DaiAJoinLike(daiJoin).dai().approve(handler, wad);
+        bytes memory calldata = abi.encodeWithSignature("daiJoin_join(address,bytes32,uint256)", bytes32(daiJoin), bytes32(handler), wad);
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+        
+        (, uint rate) = PitLike(pit).vat().ilks(ilk);
+        calldata = abi.encodeWithSignature("frob(address,bytes32,int256,int256)", bytes32(pit), ilk, 0, -int(wad * 10 ** 27 / rate));
+        CdpHandlerLike(handler).execute(cdpLib, calldata);
+    }
+
+    function lockETHAndDraw(
+        address handler,
+        address cdpLib,
+        address ethJoin,
+        address daiJoin,
+        address pit,
+        uint wadD
+    ) public payable {
+        lockETH(handler, cdpLib, ethJoin, pit);
+        draw(handler, cdpLib, daiJoin, pit, "ETH", wadD);
+    }
+
+    function openLockETHAndDraw(
+        address cdpRegistry,
+        address cdpLib,
+        address ethJoin,
+        address daiJoin,
+        address pit,
+        uint wadD
+    ) public payable returns (address handler) {
+        handler = open(cdpRegistry);
+        lockETHAndDraw(handler, cdpLib, ethJoin, daiJoin, pit, wadD);
+    }
+
+    function lockGemAndDraw(
+        address handler,
+        address cdpLib,
+        address ethJoin,
+        address daiJoin,
+        address pit,
+        bytes32 ilk,
+        uint wadC,
+        uint wadD
+    ) public payable {
+        lockGem(handler, cdpLib, ethJoin, pit, ilk, wadC);
+        draw(handler, cdpLib, daiJoin, pit, ilk, wadD);
+    }
+
+    function openLockGemAndDraw(
+        address cdpRegistry,
+        address cdpLib,
+        address ethJoin,
+        address daiJoin,
+        address pit,
+        bytes32 ilk,
+        uint wadC,
+        uint wadD
+    ) public returns (address handler) {
+        handler = open(cdpRegistry);
+        lockGemAndDraw(handler, cdpLib, ethJoin, daiJoin, pit, ilk, wadC, wadD);
+    }
+
+    function wipeAndFreeETH(
+        address handler,
+        address cdpLib,
+        address ethJoin,
+        address daiJoin,
+        address pit,
+        uint wadC,
+        uint wadD
+    ) public payable {
+        wipe(handler, cdpLib, daiJoin, pit, "ETH", wadD);
+        freeETH(handler, cdpLib, ethJoin, pit, wadC);
+    }
+
+    function wipeAndFreeGem(
+        address handler,
+        address cdpLib,
+        address gemJoin,
+        address daiJoin,
+        address pit,
+        bytes32 ilk,
+        uint wadC,
+        uint wadD
+    ) public payable {
+        wipe(handler, cdpLib, daiJoin, pit, ilk, wadD);
+        freeGem(handler, cdpLib, gemJoin, pit, ilk, wadC);
+    }
 }
--- src/DssProxy.t.sol
+++ src/DssProxy.t.sol
@@ -4,18 +4,234 @@ import "ds-test/test.sol";
 
 import "./DssProxy.sol";
 
-contract DssProxyTest is DSTest {
-    DssProxy proxy;
+import {DssDeployTest, CdpLib, CdpRegistry, CdpHandler, DSProxyFactory, DSProxy} from "mcd-cdp-handler/CdpHandler.t.sol";
+
+contract ProxyCalls {
+    DSProxy proxy;
+    DssProxy proxyLib;
+
+    function open(address) public returns (address) {
+        return address(proxy.execute(proxyLib, msg.data));
+    }
+
+    function lockETH(address, address, address, address) public payable {
+        assert(address(proxy).call.value(msg.value)(bytes4(keccak256("execute(address,bytes)")), proxyLib, uint256(0x40), msg.data.length, msg.data));
+    }
+
+    function lockGem(address, address, address, address, bytes32, uint) public {
+        proxy.execute(proxyLib, msg.data);
+    }
+
+    function freeETH(address, address, address, address, uint) public {
+        proxy.execute(proxyLib, msg.data);
+    }
+
+    function freeGem(address, address, address, address, bytes32, uint) public {
+        proxy.execute(proxyLib, msg.data);
+    }
+
+    function draw(address, address, address, address, bytes32, uint) public {
+        proxy.execute(proxyLib, msg.data);
+    }
+
+    function wipe(address, address, address, address, bytes32, uint) public {
+        proxy.execute(proxyLib, msg.data);
+    }
+
+    function lockETHAndDraw(address, address, address, address, address, uint) public payable {
+        assert(address(proxy).call.value(msg.value)(bytes4(keccak256("execute(address,bytes)")), proxyLib, uint256(0x40), msg.data.length, msg.data));
+    }
+
+    function openLockETHAndDraw(address, address, address, address, address, uint) public payable returns (address result) {
+        address target = proxy;
+        bytes memory calldata = abi.encodeWithSignature("execute(address,bytes)", bytes32(address(proxyLib)), msg.data);
+        assembly {
+            let succeeded := call(sub(gas, 5000), target, callvalue, add(calldata, 0x20), mload(calldata), 0, 0)
+            let size := returndatasize
+            let response := mload(0x40)
+            mstore(0x40, add(response, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+            mstore(response, size)
+            returndatacopy(add(response, 0x20), 0, size)
+
+            result := mload(add(response, 0x20))
+
+            switch iszero(succeeded)
+            case 1 {
+                // throw if delegatecall failed
+                revert(add(response, 0x20), size)
+            }
+        }
+    }
+
+    function lockGemAndDraw(address, address, address, address, address, bytes32, uint, uint) public {
+        proxy.execute(proxyLib, msg.data);
+    }
+
+    function openLockGemAndDraw(address, address, address, address, address, bytes32, uint, uint) public returns (address) {
+        return address(proxy.execute(proxyLib, msg.data));
+    }
+
+    function wipeAndFreeETH(address, address, address, address, address, uint, uint) public {
+        proxy.execute(proxyLib, msg.data);
+    }
+
+    function wipeAndFreeGem(address, address, address, address, address, bytes32, uint, uint) public {
+        proxy.execute(proxyLib, msg.data);
+    }
+}
+
+contract DssProxyTest is DssDeployTest, ProxyCalls {
+    CdpRegistry cdpRegistry;
+    CdpLib cdpLib;
 
     function setUp() public {
-        proxy = new DssProxy();
+        super.setUp();
+        cdpRegistry = new CdpRegistry();
+        cdpLib = new CdpLib();
+        DSProxyFactory factory = new DSProxyFactory();
+        proxyLib = new DssProxy();
+        proxy = factory.build();
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function ink(bytes32 ilk, address urn) public returns (uint inkV) {
+        (inkV,) = vat.urns(ilk, bytes32(urn));
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function testDssProxyCreateCDP() public {
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        assertEq(handler.owner(), proxy);
+        assertTrue(cdpRegistry.inRegistry(handler));
+    }
+
+    function testDssProxyLockETH() public {
+        deploy();
+        uint initialBalance = address(this).balance;
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        assertEq(ink("ETH", handler), 0);
+        this.lockETH.value(2 ether)(handler, cdpLib, ethJoin, pit);
+        assertEq(ink("ETH", handler), 2 ether);
+        assertEq(address(this).balance, initialBalance - 2 ether);
+    }
+
+    function testDssProxyLockGem() public {
+        deploy();
+        dgx.mint(5 ether);
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        dgx.approve(proxy, 2 ether);
+        assertEq(ink("DGX", handler), 0);
+        this.lockGem(handler, cdpLib, dgxJoin, pit, "DGX", 2 ether);
+        assertEq(ink("DGX", handler), 2 ether);
+        assertEq(dgx.balanceOf(this), 3 ether);
+    }
+
+    function testDssProxyfreeETH() public {
+        deploy();
+        uint initialBalance = address(this).balance;
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        this.lockETH.value(2 ether)(handler, cdpLib, ethJoin, pit);
+        this.freeETH(handler, cdpLib, ethJoin, pit, 1 ether);
+        assertEq(ink("ETH", handler), 1 ether);
+        assertEq(address(this).balance, initialBalance - 1 ether);
+    }
+
+    function testDssProxyfreeGem() public {
+        deploy();
+        dgx.mint(5 ether);
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        dgx.approve(proxy, 2 ether);
+        this.lockGem(handler, cdpLib, dgxJoin, pit, "DGX", 2 ether);
+        this.freeGem(handler, cdpLib, dgxJoin, pit, "DGX", 1 ether);
+        assertEq(ink("DGX", handler), 1 ether);
+        assertEq(dgx.balanceOf(this), 4 ether);
+    }
+
+    function testDssProxyDraw() public {
+        deploy();
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        this.lockETH.value(2 ether)(handler, cdpLib, ethJoin, pit);
+        assertEq(dai.balanceOf(this), 0);
+        this.draw(handler, cdpLib, daiJoin, pit, "ETH", 300 ether);
+        assertEq(dai.balanceOf(this), 300 ether);
+    }
+
+    function testDssProxyWipe() public {
+        deploy();
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        this.lockETH.value(2 ether)(handler, cdpLib, ethJoin, pit);
+        this.draw(handler, cdpLib, daiJoin, pit, "ETH", 300 ether);
+        dai.approve(proxy, 100 ether);
+        this.wipe(handler, cdpLib, daiJoin, pit, "ETH", 100 ether);
+        assertEq(dai.balanceOf(this), 200 ether);
+    }
+
+    function testDssProxyLockETHAndDraw() public {
+        deploy();
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        uint initialBalance = address(this).balance;
+        assertEq(ink("ETH", handler), 0);
+        assertEq(dai.balanceOf(this), 0);
+        this.lockETHAndDraw.value(2 ether)(handler, cdpLib, ethJoin, daiJoin, pit, 300 ether);
+        assertEq(ink("ETH", handler), 2 ether);
+        assertEq(dai.balanceOf(this), 300 ether);
+        assertEq(address(this).balance, initialBalance - 2 ether);
+    }
+
+    function testDssProxyOpenLockETHAndDraw() public {
+        deploy();
+        uint initialBalance = address(this).balance;
+        assertEq(dai.balanceOf(this), 0);
+        CdpHandler handler = CdpHandler(this.openLockETHAndDraw.value(2 ether)(cdpRegistry, cdpLib, ethJoin, daiJoin, pit, 300 ether));
+        assertEq(ink("ETH", handler), 2 ether);
+        assertEq(dai.balanceOf(this), 300 ether);
+        assertEq(address(this).balance, initialBalance - 2 ether);
+    }
+
+    function testDssProxyLockGemAndDraw() public {
+        deploy();
+        dgx.mint(5 ether);
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        dgx.approve(proxy, 2 ether);
+        assertEq(ink("DGX", handler), 0);
+        assertEq(dai.balanceOf(this), 0);
+        this.lockGemAndDraw(handler, cdpLib, dgxJoin, daiJoin, pit, "DGX", 2 ether, 10 ether);
+        assertEq(ink("DGX", handler), 2 ether);
+        assertEq(dai.balanceOf(this), 10 ether);
+        assertEq(dgx.balanceOf(this), 3 ether);
+    }
+
+    function testDssProxyOpenLockGemAndDraw() public {
+        deploy();
+        dgx.mint(5 ether);
+        dgx.approve(proxy, 2 ether);
+        assertEq(dai.balanceOf(this), 0);
+        CdpHandler handler = CdpHandler(this.openLockGemAndDraw(cdpRegistry, cdpLib, dgxJoin, daiJoin, pit, "DGX", 2 ether, 10 ether));
+        assertEq(ink("DGX", handler), 2 ether);
+        assertEq(dai.balanceOf(this), 10 ether);
+        assertEq(dgx.balanceOf(this), 3 ether);
+    }
+
+    function testDssProxyWipeAndFreeETH() public {
+        deploy();
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        uint initialBalance = address(this).balance;
+        this.lockETHAndDraw.value(2 ether)(handler, cdpLib, ethJoin, daiJoin, pit, 300 ether);
+        dai.approve(proxy, 250 ether);
+        this.wipeAndFreeETH(handler, cdpLib, ethJoin, daiJoin, pit, 1.5 ether, 250 ether);
+        assertEq(ink("ETH", handler), 0.5 ether);
+        assertEq(dai.balanceOf(this), 50 ether);
+        assertEq(address(this).balance, initialBalance - 0.5 ether);
+    }
+
+    function testDssProxyWipeAndFreeGem() public {
+        deploy();
+        dgx.mint(5 ether);
+        CdpHandler handler = CdpHandler(this.open(cdpRegistry));
+        dgx.approve(proxy, 2 ether);
+        this.lockGemAndDraw(handler, cdpLib, dgxJoin, daiJoin, pit, "DGX", 2 ether, 10 ether);
+        dai.approve(proxy, 8 ether);
+        this.wipeAndFreeGem(handler, cdpLib, dgxJoin, daiJoin, pit, "DGX", 1.5 ether, 8 ether);
+        assertEq(ink("DGX", handler), 0.5 ether);
+        assertEq(dai.balanceOf(this), 2 ether);
+        assertEq(dgx.balanceOf(this), 4.5 ether);
     }
 }
