ac02c87b9d6935b5d66a9230c60e6d75e1dbc8ce
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-11-23 14:34:57 -0300

    Fix copyright

--- src/DssProxyActions.sol
+++ src/DssProxyActions.sol
@@ -2,7 +2,7 @@
 
 /// DssProxyActions.sol
 
-// Copyright (C) 2018-2020 Maker Ecosystem Growth Holdings, INC.
+// Copyright (C) 2018-2021 Dai Foundation
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
