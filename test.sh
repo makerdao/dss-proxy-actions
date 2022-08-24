d1d662ebccd444f451809f604fd6eca908b9531c
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-08-24 14:03:14 -0300

    Use forge instead of dapp

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ export DAPP_BUILD_OPTIMIZE=1
 export DAPP_BUILD_OPTIMIZE_RUNS=200
 
 if [[ -z "$1" ]]; then
-    dapp --use solc:0.6.12 test -v
+    forge test -v --force --use 0.6.12 -vv
 else
-    dapp --use solc:0.6.12 test --match "$1" -vv
+    forge test -v --force --use 0.6.12 --match "$1" -vvv
 fi
